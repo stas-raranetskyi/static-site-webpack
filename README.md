ef62c5b5b22aae24e2492984aaa746d5ed32eaf3
Stas R <rar.stas@gmail.com>
2019-12-30 14:55:30 +0100

    create

---
+++ README.md
@@ -0,0 +1,5 @@
+# static site webpack
+
+1. clone or download
+2. npm i
+3. npm start or npm build
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$(function() {
+    $('body').css('color', 'blue');
+});
